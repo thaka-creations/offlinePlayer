abd9a87df8018dc922a2edfdbb887fe9b6c81b7f
kaita-john <johnkaitadocuments@gmail.com>
2025-06-03 14:56:48 +0300

    updates

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -3,8 +3,15 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace tplayer.ViewModel
 {
-    public partial class MainViewModel: ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
         string text;
+
+        [ICommand]
+        void Add()
+        {
+            Text = string.Empty;
+        }
     }
 }
